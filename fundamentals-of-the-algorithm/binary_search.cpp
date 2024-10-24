049f1493da655e5889da35d1cc9602d9fa6e45b8
Conan <13849082+conanshylock@user.noreply.gitee.com>
2024-10-25 00:55:47 +0800

    Add binary search algorithm implementation

---
+++ fundamentals-of-the-algorithm/binary_search.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+// 二分查找
+int binary_search(vector<int> &arr, int target) {
+    int head = 0, tail = arr.size() - 1;
+    while (head <= tail) {
+        int mid = (head + tail) >> 1;
+        if (arr[mid] == target) return mid;
+        else if (arr[mid] < target) head = mid + 1;
+        else tail = mid - 1;
+    }
+    return -1;
+}
+
+// 01模型二分
+int binary_search_v1(vector<int> &arr, int target) {
+    int head = 0, tail = arr.size();
+    while (head < tail) {
+        int mid = (head + tail) >> 1;
+        if (arr[mid] == target) return mid;
+        else if (arr[mid] < target) head = mid + 1;
+        else tail = mid;
+    }
+    return -1;
+}
+
+// 10模型二分
+int binary_search_v2(vector<int> &arr, int target) {
+    int head = 0, tail = arr.size() - 1;
+    while (head < tail) {
+        int mid = (head + tail + 1) >> 1;
+        if (arr[mid] == target) return mid;
+        else if (arr[mid] < target) head = mid;
+        else tail = mid - 1;
+    }
+    return -1;
+}
+
+
+int main() {
+    vector<int> arr = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    cout << binary_search(arr, 10) << endl;
+    return 0;
+}
